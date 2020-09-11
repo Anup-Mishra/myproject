3f94acdae677392b51638231c74188b732b707ce
Anup-Mishra <anupmishra.ajit@gmail.com>
2020-09-11 16:52:12 +0530

    commit

---
+++ pigments/tutorial/urls.py
@@ -0,0 +1,13 @@
+
+from django.urls import path, include
+from . import views
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'snippets', views.SnippetViewSet)
+router.register(r'users', views.UserViewSet)
+
+# The API URLs are now determined automatically by the router.
+urlpatterns = [
+    path('', include(router.urls)),
+]
